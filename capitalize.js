f8148f5ab663c85f3ed18fa0984ebebb75885a18
Fithamlak <justiceofgod01@gmail.com>
2023-02-01 01:00:25 +0300

    Add Testcase To Check If type of The Input is String

--- capitalize.js
+++ capitalize.js
@@ -1,5 +1,9 @@
 const capitalize = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+    if (typeof string === 'string' || string instanceof String) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    } else {
+        throw new Error('Iput is not string');
+    }
   };
   
   module.exports = capitalize;
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,9 @@
 const capitalize = require('./capitalize.js');
 
+test('Input is not String type', () => {
+    expect(() => stringLength(123)).toThrow();
+  });
+
 test('capitalize fithamlak is Fithamlak', () => {
     expect(capitalize('fithamlak')).toBe('Fithamlak');
   });
