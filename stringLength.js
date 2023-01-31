29cd0663d7578c420edbe93c5ed0aa21f094414f
Fithamlak <justiceofgod01@gmail.com>
2023-01-31 23:51:18 +0300

    Check If The String Is At Least 1 Character Long and Not Longer Than 10 Characters

--- stringLength.js
+++ stringLength.js
@@ -1,6 +1,8 @@
 const stringLength = (string) => {
   if (typeof string === 'string' || string instanceof String) {
+    if(string.length > 0 && string.length < 10)
     return string.length;
+    else{throw new Error('string length is 0 or greater thhan 10');}
   } else {
     throw new Error('Iput is not string length is');
   }
--- stringLength.test.js
+++ stringLength.test.js
@@ -1,5 +1,5 @@
 const stringLength = require('./stringLength.js')
-test('Input is String type', () => {
+test('Input is String type and its length is 9', () => {
     expect(() => stringLength('fithamlak').toBe(9));
   });
 
@@ -7,4 +7,12 @@ test('Input is String type', () => {
 
 test('Input is not String type', () => {
     expect(() => stringLength(12)).toThrow();
+  });
+  
+  test('length of string is 0', () => {
+    expect(() => stringLength('')).toThrow();
+  });
+  
+  test('length of string is 12', () => {
+    expect(() => stringLength('987654321237')).toThrow();
   });
